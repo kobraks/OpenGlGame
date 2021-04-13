ceab95ab5f263a30c7b656fd03f595fbba0610e6
Maciej Druciarek <kobraks2@gmail.com>
2021-04-13 04:15:15 +0200

    Changed Camera

--- OpenGlGame/Camera.h
+++ OpenGlGame/Camera.h
@@ -1,16 +1,13 @@
 #pragma once
 
-#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
 
 namespace Game
 {
 	class Camera
 	{
+	protected:
 		glm::mat4 m_Projection = glm::mat4(1.f);
-		glm::mat4 m_View = glm::mat4(1.f);
-
-		mutable bool m_ViewProjectionNeedUpdate = true;
-		mutable glm::mat4 m_ViewProjection = glm::mat4(1.f);
 	public:
 		Camera() = default;
 		Camera(const glm::mat4 &projection) : m_Projection(projection) {}
@@ -18,12 +15,5 @@ namespace Game
 		virtual ~Camera() = default;
 
 		const glm::mat4& GetProjection() const { return m_Projection; }
-		const glm::mat4& GetViewProjection() const;
-		const glm::mat4& GetView() const { return m_View; }
-
-	protected:
-		void SetProjection(const glm::mat4& projection) { m_Projection = projection; m_ViewProjectionNeedUpdate = true; }
-		void SetView(const glm::mat4& view) { m_View = view; m_ViewProjectionNeedUpdate = true; }
-	
 	};
 }
