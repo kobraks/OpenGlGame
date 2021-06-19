55f3dd5672d51646857da08f08505eebd12f5cda
Maciej Druciarek <kobraks2@gmail.com>
2021-06-19 14:30:45 +0200

    Added my own Exception class

---
+++ OpenGlGame/Exception.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <exception>
+#include <fmt/format.h>
+
+namespace Game
+{
+	class Exception : std::exception
+	{
+		std::string m_Message;
+
+	public:
+		Exception(const std::string &string) : m_Message(string) {}
+
+		template <class ...Args>
+		Exception(const std::string_view &format, Args &&... args) : m_Message(fmt::format(format, std::forward<Args>(args)...)) {}
+
+		~Exception() override = default;
+
+		const char* what() const noexcept override
+		{
+			return m_Message.c_str();
+		}
+	};
+}
