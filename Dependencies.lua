52e9936f7bc2ea5e562b052ee2c8eae89e113694
Maciej Druciarek <kobraks2@gmail.com>
2022-07-18 15:22:39 +0200

    added entt lib

--- Dependencies.lua
+++ Dependencies.lua
@@ -1,6 +1,8 @@
 BOOST = os.getenv("BOOST_DIR")
 
 IncludeDir = {}
+IncludeDir["Boost"] = "%{BOOST_DIR}"
+
 IncludeDir["Glad"] = "%{wks.location}/vendor/glad/include"
 IncludeDir["Spdlog"] = "%{wks.location}/vendor/spdlog/include"
 IncludeDir["Lua"] = "%{wks.location}/vendor/lua/src"
@@ -11,7 +13,8 @@ IncludeDir["FreeImage"] = "%{wks.location}/vendor/FreeImage/Source"
 IncludeDir["ImGui"] = "%{wks.location}/vendor/ImGui"
 IncludeDir["Glm"] = "%{wks.location}/vendor/glm"
 IncludeDir["ImGuizmo"] = "%{wks.location}/vendor/ImGuizmo"
-IncludeDir["Boost"] = "%{BOOST_DIR}"
+IncludeDir["Entt"] = "%{wks.location}/vendor/entt"
+
 
 LibraryDir = {}
 LibraryDir["Boost"] = "%{BOOST_DIR}/stage/lib"
