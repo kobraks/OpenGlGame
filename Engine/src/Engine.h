a22f6efc8aa8d08f7f51f3622fb31cc57183ee73
Maciej Druciarek <kobraks2@gmail.com>
2024-09-28 12:57:20 +0200

    Added missing include files

--- Engine/src/Engine.h
+++ Engine/src/Engine.h
@@ -7,9 +7,40 @@
 #include "Engine/Core/Log.h"
 
 #include "Engine/Core/Vector2.h"
+#include "Engine/Core/Rect.h"
 #include "Engine/Core/Time.h"
 #include "Engine/Core/Clock.h"
+#include "Engine/Core/Color.h"
 
 #include "Engine/Core/Application.h"
+#include "Engine/Core/Window.h"
 
-#include "Engine/Core/EntryPoint.h"
+#include "Engine/Core/Buffer.h"
+
+#include "Engine/Core/Image.h"
+#include "Engine/Core/KeyCode.h"
+#include "Engine/Core/MouseCodes.h"
+
+#include "Engine/Devices/Cursor.h"
+#include "Engine/Devices/Monitor.h"
+#include "Engine/Devices/Mouse.h"
+#include "Engine/Devices/Keyboard.h"
+
+#include "Engine/Events/Event.h"
+#include "Engine/Events/ApplicationEvent.h"
+#include "Engine/Events/KeyEvent.h"
+#include "Engine/Events/MouseEvent.h"
+
+#include "Engine/ImGui/ImGuiUtils.h"
+
+#include "Engine/Layers/Layer.h"
+
+#include "Engine/Renderer/Camera.h"
+#include "Engine/Renderer/Context.h"
+#include "Engine/Renderer/EditorCamera.h"
+
+#include "Engine/Scene/Components.h"
+#include "Engine/Scene/Entity.h"
+#include "Engine/Scene/SceneSerializer.h"
+#include "Engine/Scene/SceneCamera.h"
+#include "Engine/Scene/Scene.h"
