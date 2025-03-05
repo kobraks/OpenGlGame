85511bee064f2f8e438fb4c2c74ee2e3e13abca0
kobraks <kobraks2@gmail.com>
2025-03-05 23:21:58 +0100

    Added Ortho Camera

---
+++ Engine/src/Engine/Renderer/OrthographicCamera.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Engine/Core/Base.h"
+
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+
+namespace Engine {
+	class OrthographicCamera {
+	public:
+		OrthographicCamera(float left, float right, float bottom, float top);
+
+		void SetProjection(float left, float right, float bottom, float top);
+
+		const glm::vec3& GetPosition() const { return m_Position;  }
+		void SetPosition(const glm::vec3& position) { m_Position = position; RecalculateViewMatrix(); }
+
+		float GetRotation() const { return m_Rotation;  }
+		void SetRotation(float rotation) { m_Rotation = rotation; RecalculateViewMatrix(); }
+
+		const glm::mat4& GetProjectionMatrix() const { return m_ProjectionMatrix;  }
+		const glm::mat4& GetViewMatrix() const { return m_ViewMatrix;  }
+		const glm::mat4& GetViewProjectionMatrix() const { return m_ViewProjectionMatrix; }
+
+	private:
+		void RecalculateViewMatrix();
+
+	private:
+		glm::mat4 m_ProjectionMatrix;
+		glm::mat4 m_ViewMatrix;
+		glm::mat4 m_ViewProjectionMatrix;
+
+		glm::vec3 m_Position = { 0.f, 0.f, 0.f };
+		float m_Rotation = 0.f;
+	};
+}
