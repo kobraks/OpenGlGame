87eec7fe8a743fe94f82cd771e6e91204cd7d86b
kobraks <kobraks2@gmail.com>
2025-05-07 19:18:23 +0200

    Added missing functionalites

--- Engine/src/Engine/ImGui/StatisticGraphGroup.cpp
+++ Engine/src/Engine/ImGui/StatisticGraphGroup.cpp
@@ -5,18 +5,33 @@
 
 namespace Engine {
 	void StatisticGraphGroup::AddGraph(const std::string& id, Ref<StatisticGraph> graph) {
-		m_Graphs[id] = std::move(graph);
+		if (!m_GraphLookup.contains(id))
+			m_Graphs.emplace_back(id, graph);
+		m_GraphLookup[id] = graph;
 	}
 
-	void StatisticGraphGroup::DrawAll(){
-		for (auto& [id, graph] : m_Graphs) {
+	void StatisticGraphGroup::DrawAll() {
+		for (const auto& [id, graph] : m_Graphs) {
 			graph->Draw();
 			ImGui::Separator();
 		}
 	}
 
-	Ref<StatisticGraph> StatisticGraphGroup::Get(const std::string& id) {
-		auto it = m_Graphs.find(id);
-		return (it != m_Graphs.end()) ? it->second : nullptr;
+	Ref<StatisticGraph> StatisticGraphGroup::Get(const std::string& id) const {
+		auto it = m_GraphLookup.find(id);
+		return (it != m_GraphLookup.end()) ? it->second : nullptr;
+	}
+
+	bool StatisticGraphGroup::Remove(const std::string& id) {
+		auto it = m_GraphLookup.find(id);
+		if (it == m_GraphLookup.end()) return false;
+
+		m_GraphLookup.erase(it);
+		m_Graphs.erase(std::ranges::remove_if(m_Graphs, [&](const auto& pair) { return pair.first == id; }).begin());
+		return true;
+	}
+
+	bool StatisticGraphGroup::Has(const std::string& id) const {
+		return m_GraphLookup.contains(id);
 	}
 }
--- Engine/src/Engine/ImGui/StatisticGraphGroup.h
+++ Engine/src/Engine/ImGui/StatisticGraphGroup.h
@@ -2,6 +2,9 @@
 
 #include "Engine/ImGui/StatisticGraph.h"
 
+#include <string>
+#include <string_view>
+#include <vector>
 #include <unordered_map>
 
 namespace Engine {
@@ -10,9 +13,11 @@ namespace Engine {
 		void AddGraph(const std::string& id, Ref<StatisticGraph> graph);
 		void DrawAll();
 
-		Ref<StatisticGraph> Get(const std::string& id);
-
+		Ref<StatisticGraph> Get(const std::string& id) const;
+		bool Remove(const std::string& id);
+		bool Has(const std::string& id) const;
 	private:
-		std::unordered_map<std::string, Ref<StatisticGraph>> m_Graphs;
+		std::vector <std::pair<std::string, Ref<StatisticGraph>>> m_Graphs;
+		std::unordered_map<std::string, Ref<StatisticGraph>> m_GraphLookup;
 	};
 }
