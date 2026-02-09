afd8a2282c37f1d26edd2a32faf5752f3f3370cc
Maciej Druciarek <kobraks2@gmail.com>
2026-02-09 05:38:29 +0100

    Refactor AssetMetadata accessors and initialization
    - Initialize TimeStamp to 0 by default.
    - Use both TransparentStringHash and TransparentStringEqual for UserVariables map.
    - Fix GetOr and Get to use find() instead of begin().
    - Split operator[] into const and non-const versions, returning copy or reference as appropriate, and add At/At const helpers.
    - Correct operator bool() to return true when Type is not None.

--- Engine/src/Engine/Asset/AssetMetadata.h
+++ Engine/src/Engine/Asset/AssetMetadata.h
@@ -9,12 +9,12 @@ namespace Engine {
 	struct AssetMetadata {
 		AssetType Type = AssetType::None;
 		std::filesystem::path ImportPath;
-		uint64_t TimeStamp;
-		std::unordered_map<std::string, std::any, TransparentStringHash> UserVariables;
+		uint64_t TimeStamp = 0;
+		std::unordered_map<std::string, std::any, TransparentStringHash, TransparentStringEqual> UserVariables;
 
 		template<typename T>
 		T GetOr (std::string_view index, const T& defaultValue) const {
-			const auto it = UserVariables.begin();//UserVariables.find(index);
+			const auto it = UserVariables.find(index);
 			if (it != UserVariables.end())
 				return std::any_cast<T>(it->second);
 
@@ -23,21 +23,37 @@ namespace Engine {
 
 		template<typename T>
 		T Get(std::string_view index) const {
-			const auto it = UserVariables.begin();
+			const auto it = UserVariables.find(index);
 			if (it != UserVariables.end())
 				return std::any_cast<T>(it->second);
 
 			throw std::out_of_range("Key not found in UserVariables");
 		}
 
-		std::any operator[](std::string_view index) const {
-			const auto it = UserVariables.begin();
+		std::any& At(std::string_view index) {
+			const auto it = UserVariables.find(index);
 			if (it != UserVariables.end())
 				return it->second;
 
 			throw std::out_of_range("Key not found in UserVariables");
 		}
 
-		operator bool() const { return Type == AssetType::None; }
+		std::any& operator[](std::string_view index) {
+			return At(index);
+		}
+
+		std::any At(std::string_view index) const {
+			const auto it = UserVariables.find(index);
+			if (it != UserVariables.end())
+				return it->second;
+
+			throw std::out_of_range("Key not found in UserVariables");
+		}
+
+		std::any operator[](std::string_view index) const {
+			return At(index);
+		}
+
+		operator bool() const { return Type != AssetType::None; }
 	};
 }
